1cb9ee7d1d9dbcd86bb1a217dcfb101fc080c7e0
Thomabed Thomabed <thomabed@yahoo.com>
2023-04-13 19:11:58 +0000

    changed border line

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -25,7 +25,17 @@
     }
     .layout {
         width: 100%;
-        max-width:900px;
+        max-width:  1024px;
         margin:0 auto;
+        display:grid;
+        grid-template-rows: auto 1fr;
+        align-items: center;
+    }
+
+    .header,
+    .content {
+        background-color: white;
+        border:4px solid black;
+        padding: 12px 24px;
     }
 </style>
